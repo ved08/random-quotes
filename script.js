82b155244fd4dce725549c82046be73579518185
Vedvardhan <vedvardhan.g@gmail.com>
2020-06-16 23:12:21 +0530

    Added Transitions

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let body = document.body;
+let button = document.getElementById('click');
+let container = document.getElementById('container');
 show();
 function show(){
   let output = document.getElementById('quote');
@@ -5,9 +8,18 @@ function show(){
   fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
   .then(res => res.json())
   .then(resp => {
-    console.log(resp)
     let randomNumber = Math.floor(Math.random() * 102)
     output.textContent = resp.quotes[randomNumber].quote
     author.textContent = '- ' + resp.quotes[randomNumber].author
   })
+  let col1 = generateColor();
+  let col2 = generateColor();
+  let col3 = generateColor();
+  let color = `rgb(${col1}, ${col2}, ${col3})`;
+  body.style.background = color;
+  button.style.background = color;
+  container.style.color = color;
+}
+function generateColor() {
+  return Math.floor(Math.random() * 255)
 }
